a5b5569db940711f6ddb5903ccaf5cc84f3bf090
Gagan Tuli <gagan1825.cse18@chitkara.edu.in>
2021-03-23 10:54:20 +0530

    Create special stack.java
    without using another stack

---
+++ special stack.java
@@ -0,0 +1,64 @@
+class GfG{
+    int min;
+	public void push(int a,Stack<Integer> s)
+	{
+	    //add code here.
+	    if(s.isEmpty()){
+	        min = a;
+	        s.push(a);
+	        return ;
+	    }
+	    else
+	    {
+	        if(a<min){
+	            s.push( (2*a - min) );
+	            min = a;
+	        }
+	        else
+	        {
+	            s.push(a);
+	        }
+	    }
+	}
+	public int pop(Stack<Integer> s)
+    {
+            //add code here.
+            if(s.isEmpty()){
+                return -1;
+            }
+            else
+            {
+                int x = s.pop();
+                if(x<min){
+                    min = 2*min - x;
+                }
+                return x;
+            }
+        
+            
+	}
+	public int min(Stack<Integer> s)
+    {
+           //add code here.
+           if(!s.isEmpty())
+           return min;
+           else
+           return -1;
+	}
+	public boolean isFull(Stack<Integer>s, int n)
+    {
+           //add code here.
+           if(s.size()==n)
+           return true;
+           else
+           return false;
+	}
+	public boolean isEmpty(Stack<Integer>s)
+    {
+           //add code here.
+           if(s.isEmpty())
+             return true;
+             else
+             return false;
+	}
+}
